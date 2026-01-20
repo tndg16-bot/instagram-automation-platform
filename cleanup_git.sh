c7ae4d7588dc867cdccf691dce9324c35b7e6793
Takahiro Motoyama <t.ndc16@gmail.com>
2026-01-20 16:51:04 +0900

    chore: final cleanup of temporary files

--- cleanup_git.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-git status
-git add -A
-git commit -m "chore: clean up temporary files and directories
-
-- Remove accidental docker/ directory
-- Remove testing scripts
-- Clean up git working directory"
-git push origin main
---
+++ final_cleanup.sh
@@ -0,0 +1,4 @@
+rm final_summary.sh
+git add -A
+git commit -m "chore: final cleanup of temporary files"
+git push origin main
